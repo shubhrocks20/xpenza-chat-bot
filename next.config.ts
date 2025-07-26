5e97753d3c598cc22206b159ad345353818047b4
Shubham Kumar <Shubhamkumar2056@gmail.com>
2025-07-26 12:21:56 +0530

    fix: remove unnecessary comment in parentUrl declaration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
+/** Replace this with your actual React app domain (no trailing slash) */
+const REACT_APP_DOMAIN = process.env.XPENZA_FRONTEND_URI;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // applies to all routes
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: `ALLOW-FROM ${REACT_APP_DOMAIN}`,
+          },
+          {
+            key: "Content-Security-Policy",
+            value: `frame-ancestors 'self' ${REACT_APP_DOMAIN};`,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
